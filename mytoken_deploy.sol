60ee71f39b32bdd6fe1ac4133f96cdfc34bc0a8a
Drag_shreyas <90703376+Dankflee@users.noreply.github.com>
2023-08-30 21:07:46 +0530

    Update mytoken_deploy.sol

--- mytoken_deploy.sol
+++ mytoken_deploy.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract mytoken_deploy is ERC20{
-    
+contract mytoken_deploy is ERC20 {
     string public Name;
     string public Symbol;
     uint256 public totalsupply;
@@ -17,29 +16,31 @@ contract mytoken_deploy is ERC20{
         _;
     }
 
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name,_symbol) {
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
         Name = _name;
         Symbol = _symbol;
         totalsupply = _initialSupply;
         owner = msg.sender;
     }
-    function mint(address account, uint256 amount) public   {
-        _balances[msg.sender]=amount;
+
+    function mint(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
     }
-    
-        function burn(address account, uint256 amount) public   {
-        _balances[msg.sender]=_balances[msg.sender]-amount;
+
+    function burn(address account, uint256 amount) public onlyOwner {
         _burn(account, amount);
     }
+
     function transfer(address from, address to, uint256 amount) public {
-         _balances[from]=_balances[from]-amount;
-         _balances[to]=_balances[to]+amount;
-         transfer(to, amount);
-     }
-         function balanceOf(address account) public view virtual override  returns (uint256) {
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        transfer(to, amount);
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
         return _balances[account];
     }
+
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _approve(sender, msg.sender, allowance(sender, msg.sender) - amount);
         _balances[sender] -= amount;
@@ -47,5 +48,4 @@ contract mytoken_deploy is ERC20{
         emit Transfer(sender, recipient, amount);
         return true;
     }
-
 }
